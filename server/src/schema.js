648994557d2fab404d9f74c2b1b0b6dcf75bd57c
Jorge Martinez <martinezjorge273@gmail.com>
2020-11-04 10:57:37 -0600

    Added LaunchConnection wrapper around Launches for pagination

--- server/src/schema.js
+++ server/src/schema.js
@@ -28,9 +28,28 @@ const typeDefs = gql`
     }
 
     type Query { 
-        launches: [Launch]!
+        launches:(
+            """
+            The number of results to show. Must be >= 1. Default = 20.
+            """
+            pageSize: Int 
+            """
+            If you add a cursor here, it will only return results _after_ this cursor.
+            """
+            after: String) LaunchConnection!
         launch(id: ID!): Launch
         me: User 
+    }
+
+    """
+    Simple wrapper around our list of launches that ocntains a cursor to the
+    last item in the list. Pass this cursor to the launches query to fetch results
+    after these.
+    """
+    type LaunchConnection {
+        cursor: String!
+        hasMore: Boolean!
+        launches: [Launch]!
     }
 
     type Mutation { 
