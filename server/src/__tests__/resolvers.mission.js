6badea4d5288e2ae77cdc7708ab84b440fbb757d
Jorge Martinez <martinezjorge273@gmail.com>
2020-11-04 23:47:15 -0600

    Added unit tests for mission resolvers

---
+++ server/src/__tests__/resolvers.mission.js
@@ -0,0 +1,22 @@
+const { isTerminating } = require('apollo-link/lib/linkUtils');
+const resolvers = require('../resolvers');
+
+const mockMission = {
+    name: 'foo',
+    missionPatchLarge: 'LG',
+    missionPatchSmall: 'SM',
+};
+
+describe('[Mission.missionPatch]', () => {
+    it('chooses the right sized patch', () => {
+        const { missionPatch } = resolvers.Mission;
+
+        const resDefault = missionPatch(mockMission);
+        const resSmall = missionPatch(mockMission, { size: 'SMALL' });
+        const resLarge = missionPatch(mockMission, { size: 'LARGE' });
+
+        expect(resDefault).toEqual('LG');
+        expect(resSmall).toEqual('SM');
+        expect(resLarge).toEqual('LG');
+    });
+});
